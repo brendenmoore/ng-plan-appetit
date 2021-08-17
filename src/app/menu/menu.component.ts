0762d8673c29b2506a0bf9ad31d299ee49b6f700
Brenden Moore <52762708+brendenmoore@users.noreply.github.com>
2021-08-17 19:35:39 -0400

    upgraded menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,7 @@
+import { CdkDragDrop, copyArrayItem, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { isNamedTupleMember } from 'typescript';
 import { Meal, MenuDay, Template } from '../models';
 import { MealService } from '../services/meal.service';
 import { MenuService } from '../services/menu.service';
@@ -13,16 +15,26 @@ export class MenuComponent implements OnInit {
   daysInView: Date[];
   menuDays: MenuDay[] | undefined;
   template: Template | undefined;
+  menu: { date: Date; menuDay: MenuDay }[] = [];
 
   constructor(
     private menuService: MenuService,
     private mealService: MealService
   ) {
     this.daysInView = this.menuService.getCurrentWeek();
-    const firstDay = this.daysInView[0]
-    const numberOfDays = this.daysInView.length
-    this.menuService.getMenuDaysInRange(firstDay, numberOfDays).valueChanges().subscribe(menuDays => this.menuDays = menuDays)
-    this.mealService.getTemplate().subscribe(template => {this.template = template; console.log(template)});
+    const firstDay = this.daysInView[0];
+    const numberOfDays = this.daysInView.length;
+    this.menuService
+      .getMenuDaysInRange(firstDay, numberOfDays)
+      .valueChanges()
+      .subscribe((menuDays) => {
+        this.menuDays = menuDays;
+        this.loadMenu();
+      });
+    this.mealService.getTemplate().subscribe((template) => {
+      this.template = template;
+      console.log(template);
+    });
   }
 
   ngOnInit(): void {}
@@ -32,7 +44,30 @@ export class MenuComponent implements OnInit {
   }
 
   loadMenu() {
-    if (this.menuDays && this.template)
-    this.menuService.loadMenu(this.daysInView, this.menuDays, this.template)
+    if (this.menuDays) {
+      this.menu = this.menuService.loadMenu(this.daysInView, this.menuDays);
+      this.menuDays = this.menu.map(item => item.menuDay)
+    }
+    console.log(this.menu);
+  }
+
+  drop(event: CdkDragDrop<any>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data.recipes,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data.recipes,
+        event.container.data.recipes,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+    console.log(this.menu);
+    console.log(this.menuDays);
+    this.menuService.updateMultipleDays(this.menu.map(item => item.menuDay))
   }
 }
--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -3,6 +3,7 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 import { Meal, Menu, MenuDay, Template } from '../models';
 import { UserService } from '../user.service';
 import {endOfWeek, startOfWeek, eachDayOfInterval, format} from 'date-fns'
+import { newMeal } from './util';
 
 @Injectable({
   providedIn: 'root',
@@ -19,6 +20,21 @@ export class MenuService {
 
   setMenuDay(menuDay: MenuDay){
     return this.menu.doc(menuDay.dateString).set(menuDay)
+  }
+
+  updateMultipleDays(menuDays: MenuDay[] | undefined) {
+    if (!menuDays) {
+      return;
+    }
+
+    let batch = this.store.firestore.batch();
+
+    menuDays.forEach(menuDay => {
+      let ref = this.menu.doc(menuDay.dateString).ref;
+      batch.set(ref, menuDay)
+    })
+
+    return batch.commit()
   }
 
   addMenuDay(day: Date, meal: Meal){
@@ -44,8 +60,20 @@ export class MenuService {
     return this.store.collection<MenuDay>('users/' + this.userId + "/menuDays", ref => ref.orderBy('dateString').startAt(startingDayString).limit(numberOfDays))
   }
 
-  loadMenu(daysInView: Date[], menuDays: MenuDay[], template: Template){
-    daysInView
+  loadMenu(daysInView: Date[], menuDays: MenuDay[]){
+    let menu = daysInView.map(date => {
+      let menuDay = menuDays.find(menu => menu.dateNumber === date.getTime())
+      if (!menuDay) {
+        const dateString = format(date, this.DATE_FORMAT);
+        menuDay = {
+          dateString: dateString,
+          dateNumber: date.getTime(),
+          meal: newMeal(),
+        };
+      }
+      return {date: date, menuDay: menuDay}
+    })
+    return menu
   }
 
 }
