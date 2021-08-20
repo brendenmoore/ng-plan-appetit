e8bd323649b996da6ba771d08290a636985af567
Brenden Moore <52762708+brendenmoore@users.noreply.github.com>
2021-08-20 16:41:39 -0400

    recipe styling

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,5 +1,5 @@
 export interface Recipe {
-  id?: string;
+  id: string;
   name: string;
   createdOn: number;
 }
--- src/app/recipes/recipe-stack-view/recipe-stack-view.component.ts
+++ src/app/recipes/recipe-stack-view/recipe-stack-view.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Recipe, RecipeDetails } from 'src/app/models';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-stack-view',
   templateUrl: './recipe-stack-view.component.html',
-  styleUrls: ['./recipe-stack-view.component.css']
+  styleUrls: ['./recipe-stack-view.component.css'],
 })
 export class RecipeStackViewComponent implements OnInit {
+  @Input() recipe?: Recipe;
+  recipeDetails$?: Observable<RecipeDetails | undefined>
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
+    if (this.recipe) {
+      this.loadData(this.recipe)
+    }
   }
 
+  loadData(recipe: Recipe) {
+    this.recipeDetails$ = this.recipeService
+      .getRecipeDetails(recipe.id)
+      .valueChanges();
+  }
 }
